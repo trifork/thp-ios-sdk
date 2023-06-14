c5a28ac48146bd5434dd068f94a34d321069421d
Nicolai Harbo <niha@trifork.com>
2023-06-14 11:04:13 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TriforkHealthPlatformSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TriforkHealthPlatformSDK",
+            targets: ["TriforkHealthPlatformSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/trifork/TIM-iOS", from: "2.8.2"),
+        .package(url: "https://github.com/apollographql/apollo-ios.git", from: "1.0.5")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TriforkHealthPlatformSDK",
+            dependencies: [
+                .product(name: "TIM", package: "TIM-iOS"),
+                .product(name: "Apollo", package: "apollo-ios")
+            ]
+        ),
+        .testTarget(
+            name: "TriforkHealthPlatformSDKTests",
+            dependencies: [
+                "TriforkHealthPlatformSDK"
+            ]
+        ),
+    ]
+)
---
+++ Tests/TriforkHealthPlatformSDKTests/thp_ios_sdkTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import TriforkHealthPlatformSDK
+
+final class thp_ios_sdkTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
