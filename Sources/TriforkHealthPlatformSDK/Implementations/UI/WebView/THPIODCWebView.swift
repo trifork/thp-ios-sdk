d392d5be4bab4e71c737b57a550ed66c582acd23
Nicolai Harbo <niha@trifork.com>
2023-08-29 10:03:06 +0200

    added webview

--- Sources/TriforkHealthPlatformSDK/Implementations/UI/WebView/THPIODCWebView.swift
+++ Sources/TriforkHealthPlatformSDK/Implementations/UI/WebView/THPIODCWebView.swift
@@ -10,15 +10,29 @@ import SwiftUI
 public struct THPIODCWebView: View {
     
     public let urlRequest: URLRequest
+    @State private var isLoading = true
+    @State private var error: Error? = nil
     
     public init(urlRequest: URLRequest) {
         self.urlRequest = urlRequest
     }
     
     public var body: some View {
-        VStack {
-            Text("This is what we want to load:")
-            Text(urlRequest.url?.absoluteString ?? ".. no url")
+        ZStack {
+            if let error = error {
+                Text(error.localizedDescription)
+            } else {
+                WebView(
+                    urlRequest: urlRequest,
+                    isLoading: $isLoading,
+                    error: $error
+                )
+                
+                if isLoading {
+                    ProgressView()
+                }
+            }
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
---
+++ Sources/TriforkHealthPlatformSDK/Implementations/UI/WebView/WebView.swift
@@ -0,0 +1,83 @@
+//
+//  File.swift
+//  
+//
+//  Created by Nicolai Harbo on 29/08/2023.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlRequest: URLRequest
+    @Binding var isLoading: Bool
+    @Binding var error: Error?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let preferences = WKPreferences()
+        preferences.javaScriptCanOpenWindowsAutomatically = false
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences = preferences
+        
+        let webView = WKWebView(frame: CGRect.zero, configuration: configuration)
+        webView.uiDelegate = context.coordinator
+        webView.navigationDelegate = context.coordinator
+//        webView.allowsBackForwardNavigationGestures = true
+        webView.scrollView.isScrollEnabled = true
+        
+        // Clear all cookies, just for good measure's sake
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            WKWebsiteDataStore.default().removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), for: records) {
+                webView.load(urlRequest)
+            }
+        }
+        
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {}
+    
+    // MARK: - Coordinator
+    class Coordinator : NSObject, WKNavigationDelegate, WKUIDelegate {
+        
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        // MARK: - WKNavigationDelegate
+        
+        func webView(
+            _ webView: WKWebView,
+            decidePolicyFor navigationAction: WKNavigationAction,
+            decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
+        ) {
+            decisionHandler(.allow)
+        }
+        
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            parent.isLoading = true
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.isLoading = false
+        }
+        
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            parent.isLoading = false
+            parent.error = error
+        }
+        
+        // MARK: - WKUIDelegate
+        
+    }
+}
