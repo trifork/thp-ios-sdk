e4cfb9413d15e3d8abfe433aae6d7f827e12fd86
Pere Daniel Prieto <ppm@trifork.com>
2026-01-12 15:00:23 +0100

    Added parsing of "Error" to "THPError".

--- Sources/TriforkHealthPlatformSDK/Data/Models/THPError.swift
+++ Sources/TriforkHealthPlatformSDK/Data/Models/THPError.swift
@@ -6,6 +6,15 @@ public typealias THPAuthError = TIMAuthError
 public typealias THPStorageError = TIMStorageError
 public typealias THPEncryptedStorageError = TIMEncryptedStorageError
 
+extension Error {
+    func asTHPError() -> THPError {
+        guard let timError = self as? TIMError else {
+            return .unknown
+        }
+        return timError.asTHPError()
+    }
+}
+
 extension TIMError {
     func asTHPError() -> THPError {
         switch self {
@@ -20,6 +29,7 @@ extension TIMError {
 public enum THPError: Error {
     case auth(THPAuthError)
     case storage(THPStorageError)
+    case unknown
 
     public var errorDescription: String? {
         switch self {
@@ -27,6 +37,8 @@ public enum THPError: Error {
             error.localizedDescription
         case .storage(let error):
             error.localizedDescription
+        case .unknown:
+            "An unexpected error occurred that cannot be identified."
         }
     }
 }
