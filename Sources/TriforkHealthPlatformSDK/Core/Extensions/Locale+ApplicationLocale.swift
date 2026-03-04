a0edc5c6356f6639824e5685dcd3d52ed99192df
Pere Daniel Prieto <ppm@trifork.com>
2026-03-04 09:44:39 +0100

    Added "additionalParameters" dictionary parameter to THPConfiguration. Updated documentation.

--- Sources/TriforkHealthPlatformSDK/Core/Extensions/Locale+ApplicationLocale.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension Locale {
-    public static var applicationLocale: Locale {
-        Bundle.main.preferredLocalizations.first.flatMap { Locale(identifier: $0) } ?? Locale.current
-    }
-}
--- Sources/TriforkHealthPlatformSDK/Data/Models/THPConfiguration.swift
+++ Sources/TriforkHealthPlatformSDK/Data/Models/THPConfiguration.swift
@@ -8,7 +8,8 @@ public struct THPConfiguration: Sendable {
     public let clientId: String
     public let redirectUrl: String
     public let loginFlowKey: String
-    
+    public let additionalParameters: [String: String]
+
     /// Default constructor
     /// - Parameters:
     ///   - scopes: Scopes, e.g. `["scope"]`
@@ -17,13 +18,15 @@ public struct THPConfiguration: Sendable {
     ///   - redirectUrl: Redirect URL, e.g. `"my-app:/"`
     ///   - baseAuthURL: Base base URL to authenticate, e.g. https://trifork.com
     ///   - loginFlowKey: The client app key to identify the login flow source, e.g. `"client-flow"`
+    ///   - additionalParameters: Additional configuration parameters, such as the prompt or the locale for the UI.
     public init(
         scopes: [String],
         realm: String,
         clientId: String,
         redirectUrl: String,
         baseAuthURL: URL,
-        loginFlowKey: String
+        loginFlowKey: String,
+        additionalParameters: [String: String]
     ) {
         self.scopes = scopes
         self.realm = realm
@@ -31,5 +34,6 @@ public struct THPConfiguration: Sendable {
         self.redirectUrl = redirectUrl
         self.baseAuthURL = baseAuthURL
         self.loginFlowKey = loginFlowKey
+        self.additionalParameters = additionalParameters
     }
 }
